d16a13939831ebdb591de035b248eba90e98500b
Tiantian Liu <tobias_liu@outlook.com>
2023-11-14 21:21:43 +0100

    implement streamlit

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from tetras import Tetra
 import functions
 from openai import OpenAI
+import streamlit as st
 
+with st.sidebar:
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="chatbot_api_key", type="password"
+    )
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/Hornet47/Tetra)"
 
-def main():
-    client = OpenAI().beta
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key).beta
     tetra = Tetra(
         client=client,
         tools=[
@@ -15,10 +37,9 @@ def main():
             },
         ],
     )
-    response = tetra.process_message("What is the weather in Shanghai today?")
-    print(response)
-    return
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-
-if __name__ == "__main__":
-    main()
+    msg = tetra.process_message(prompt)
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
