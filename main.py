fe585a6716862bf8470000e63cd9b8e690fd6f6f
Tiantian Liu <tobias_liu@outlook.com>
2023-11-21 03:05:01 +0100

    changed main file name

---
+++ main.py
@@ -0,0 +1,23 @@
+from agents import Agent
+from openai import OpenAI
+import user_proxy
+import utils
+import json
+
+def main():
+    config_file_path = 'assistant_config.json'
+
+    with open(config_file_path, 'r') as file:
+        config = json.load(file)
+
+    code_writer = Agent(
+        **config["PostgreSQL_writer"]
+    )
+    msg = code_writer.process_message("Generate the query for: the most popular film category in China")
+    query = utils.extract_sql(msg)
+    print(query)
+    res = user_proxy.run_sql(query=query)
+    print(res)
+    
+if __name__ == "__main__":
+    main()
